27e2279eb6a86fcd3701827185e9269f9817460f
c-wrenn <crislynwren@yahoo.com>
2023-11-30 03:01:03 -0600

    "webpack config"

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -20,12 +20,59 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'cautious-meme',
+      }),
+       // will inject our custom service worker
+       new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+            // Creates a manifest.json file.
+            new WebpackPwaManifest({
+              fingerprints: false,
+              inject: true,
+              name: "Jate",
+              short_name: "J.A.T.E",
+              description: "This is a text editor that runs on different versions",
+              background_color: "#225ca3",
+              theme_color: "#225ca3",
+              start_url: "./",
+              id: '/',
+              publicPath: "./",
+              icons: [
+                {
+                  src: path.resolve("src/images/logo.png"),
+                  sizes: [96, 128, 192, 256, 384, 512],
+                  destination: path.join("assets", "icons"),
+                },
+              ],
+            }),
       
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
